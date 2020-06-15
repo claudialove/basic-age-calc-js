f41901e9f3aa039faadaddb21813f8c0c178f31a
Claudia Petty <claudialove32@gmail.com>
2020-06-15 17:22:28 -0400

    bug fixes

--- age-calc.js
+++ age-calc.js
@@ -1,6 +1,6 @@
 var agePrompt = prompt("What is your age");
-var days = age * 365;
-alert(`At your age: ${age} you have been alive for ${days} days!!`)
+var days = agePrompt * 365;
+alert(`At your age: ${agePrompt} you have been alive for ${days} days!!`)
 
 
 
